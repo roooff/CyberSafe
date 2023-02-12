1644b5fa013b4323aa96adf6635781e209b86b6e
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2023-02-12 22:27:43 +0200

    AAAAAAAAAAAAAAAA

--- Site ver 11.0/CyberSafe/Areas/Identity/Data/CyberSafeDBContext.cs
+++ Site ver 11.0/CyberSafe/Areas/Identity/Data/CyberSafeDBContext.cs
@@ -1,7 +1,9 @@
 ﻿using CyberSafe.Areas.Identity.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace CyberSafe.Areas.Identity.Data;
 
@@ -18,5 +20,15 @@ public class CyberSafeDBContext : IdentityDbContext<CyberSafeUser>
         // Customize the ASP.NET Identity model and override the defaults if needed.
         // For example, you can rename the ASP.NET Identity table names and more.
         // Add your customizations after calling base.OnModelCreating(builder);
+        builder.ApplyConfiguration(new CyberSafeUserIdentityConfiguration());
+    }
+}
+
+public class CyberSafeUserIdentityConfiguration : IEntityTypeConfiguration<CyberSafeUser>
+{
+    public void Configure(EntityTypeBuilder<CyberSafeUser> builder)
+    {
+        builder.Property(u => u.FirstName).HasMaxLength(255);
+        builder.Property(u => u.LastName).HasMaxLength(255);
     }
 }
