93b1a864a7f2bfde720a9a11da4224cbf0689a64
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2023-02-12 21:44:48 +0200

    AAAAAAAAAAAAAAAAAAAAAAA

--- Site ver 11.0/Site ver 11.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Site_ver_11._0.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
