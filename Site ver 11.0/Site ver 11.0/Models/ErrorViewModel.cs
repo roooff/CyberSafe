17fdd977c1f10030c486f693f6b667f509db130b
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2023-02-12 21:11:19 +0200

    SHTE SE SAMOUBIQ

---
+++ Site ver 11.0/Site ver 11.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Site_ver_11._0.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
