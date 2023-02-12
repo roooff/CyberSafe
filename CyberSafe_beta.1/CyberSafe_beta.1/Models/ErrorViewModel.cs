aca3d407f7ebeee3d45ffcf7b21d18be0b2f3d38
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2023-02-12 21:07:06 +0200

    Deleting

--- CyberSafe_beta.1/CyberSafe_beta.1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CyberSafe_beta._1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
