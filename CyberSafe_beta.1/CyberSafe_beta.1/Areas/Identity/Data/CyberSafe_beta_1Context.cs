7af98ee5a37609737f6320aecc46945a527d1cc7
Raffy Nikoghosyan <114150832+roooff@users.noreply.github.com>
2023-02-12 20:46:30 +0200

    rerere

---
+++ CyberSafe_beta.1/CyberSafe_beta.1/Areas/Identity/Data/CyberSafe_beta_1Context.cs
@@ -0,0 +1,22 @@
+﻿using CyberSafe_beta._1.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CyberSafe_beta._1.Data;
+
+public class CyberSafe_beta_1Context : IdentityDbContext<CyberSafe_beta_1User>
+{
+    public CyberSafe_beta_1Context(DbContextOptions<CyberSafe_beta_1Context> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- CyberSafe_beta.1/CyberSafe_beta.1/Program.cs
+++ CyberSafe_beta.1/CyberSafe_beta.1/Program.cs
@@ -1,4 +1,16 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using CyberSafe_beta._1.Data;
+using CyberSafe_beta._1.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("CyberSafe_beta_1ContextConnection") ?? throw new InvalidOperationException("Connection string 'CyberSafe_beta_1ContextConnection' not found.");
+
+builder.Services.AddDbContext<CyberSafe_beta_1Context>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<CyberSafe_beta_1User>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<CyberSafe_beta_1Context>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +29,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
